78cb3443b6ae4138cecd759737dea40cbb801c27
Jasper Bruin <jasperbruin@me.com>
2024-05-10 17:57:52 +0200

    Corrected paths for CI

--- src/lib_version/version.py
+++ src/lib_version/version.py
@@ -6,14 +6,10 @@ class VersionUtil:
     @staticmethod
     def get_version():
         try:
-            with open("version.py", "r") as file:
+            with open("src/lib_version/version.py", "r") as file:
                 version_line = file.readline()
                 version = version_line.split("'")[1]
                 return version
         except FileNotFoundError:
             return "Version file not found."
 
-
-# Usage
-version_util = VersionUtil()
-print("Library Version:", version_util.get_version())
